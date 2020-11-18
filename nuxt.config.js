2f1c44baa1bbe761b6f6c73b164a42c5034e2d48
Takane Ichinose <ichinose.takane@gmail.com>
2020-11-18 09:54:35 +0900

    Build to static

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,11 +4,12 @@ export default {
     extend(config, ctx) {
       config.module.rules.push({
         test: /\.(ogg|mp3|wav|mpe?g)$/i,
-        loader: 'file-loader',
+        loader: "file-loader",
         options: {
-          name: '[path][name].[ext]'
+          name: "[path][name].[ext]"
         }
       })
     }
-  }
+  },
+  target: "static"
 };
